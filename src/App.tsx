386e90a6d395cc9e7f41da5371428c0a3d1442b8
chspower1 <chspower1@gmail.com>
2023-01-28 19:08:19 +0900

    feat: router 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+import Detail from "./pages/Detail";
+import Home from "./pages/Home";
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
