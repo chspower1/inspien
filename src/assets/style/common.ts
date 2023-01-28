5689a79246d954c0401e5bfe04d071b86aa6f33e
chspower1 <chspower1@gmail.com>
2023-01-28 23:03:57 +0900

    style : styled-components 세팅

---
+++ src/assets/style/common.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Row = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const Col = styled(Row)`
+  flex-direction: column;
+`;
