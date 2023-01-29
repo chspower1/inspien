8fca6af124bb25ea01ae1489be4636e26b6a6f5a
chspower1 <chspower1@gmail.com>
2023-01-30 01:46:26 +0900

    style : css reset

--- src/assets/style/GlobalStyles.ts
+++ src/assets/style/GlobalStyles.ts
@@ -1,6 +1,36 @@
 import { createGlobalStyle } from "styled-components";
 import { reset } from "styled-reset";
+import "../css/font.css";
 export const GlobalStyled = createGlobalStyle`
     ${reset}
-    
+    :focus {
+        outline: none;
+        border: none;
+    }
+    ::-webkit-scrollbar {
+        display: none;
+    }
+    *{
+        font-family: "Sebang";      
+        font-size: 16px;
+    }
+    html{
+        font-size: 16px;
+        -webkit-text-size-adjust: none;
+        font-family: "Sebang";       
+        font-display: fallback;
+        -ms-overflow-style: none;
+        scrollbar-width: none;
+    }
+    button {
+        background: none;
+        padding: 0;
+        border: none;
+        cursor: pointer;
+        &:disabled {
+            cursor: default;
+            fill: #f2f3f4;
+        }
+    }
+
 `;
