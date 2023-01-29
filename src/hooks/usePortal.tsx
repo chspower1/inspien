e759662e525ec2f8db040eeed867fc6ad1cabdc5
chspower1 <chspower1@gmail.com>
2023-01-29 23:17:11 +0900

    feat(usePortal) : Modal mount 로직 변경

---
+++ src/hooks/usePortal.tsx
@@ -0,0 +1,28 @@
+import { ReactNode, useState } from "react";
+import ReactDOM from "react-dom";
+import styled from "styled-components";
+interface ModalProps {
+  children: ReactNode | JSX.Element;
+}
+const usePortal = () => {
+  const [isMount, setIsMount] = useState(false);
+
+  const Portal = ({ children }: ModalProps) => {
+    return isMount
+      ? ReactDOM.createPortal(
+          <Overlay>{children}</Overlay>,
+          document.getElementById("modal-root") as HTMLElement
+        )
+      : null;
+  };
+  return { Portal, isMount, setIsMount };
+};
+
+export default usePortal;
+const Overlay = styled.div`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+`;
