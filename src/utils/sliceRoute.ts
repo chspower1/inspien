350c82a04147ab315d87caeb20d3cda11688a913
chspower1 <chspower1@gmail.com>
2023-02-01 02:01:13 +0900

    feat: sliceRoute 예외처리

--- src/utils/sliceRoute.ts
+++ src/utils/sliceRoute.ts
@@ -1,4 +1,14 @@
-export const sliceRoute = (fullRoute: string) => {
+interface SliceRouteProps {
+  currentParent: string | undefined;
+  currentDir: string;
+}
+export const sliceRoute = ({ currentParent, currentDir }: SliceRouteProps) => {
+  const fullRoute =
+    currentParent === undefined
+      ? "/"
+      : currentParent === "/"
+      ? currentParent + currentDir
+      : currentParent + "/" + currentDir;
   if (fullRoute === "/") return [fullRoute];
   else {
     const routeArr = fullRoute.split("/");
