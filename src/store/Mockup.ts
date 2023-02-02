e6cc8c50a7a15417ba7ba9e1c3047121f6834132
chspower1 <chspower1@gmail.com>
2023-02-02 16:13:43 +0900

    feat : MockupData 형식 변경

--- src/store/Mockup.ts
+++ src/store/Mockup.ts
@@ -5,95 +5,103 @@ export const MockupState: MockupDate = {
     {
       id: 1,
       serverName: "Server #1",
-      directories: {
-        name: "/",
-        type: "DIRECTORY",
-        parent: undefined,
-        children: [
-          {
-            name: "home",
-            type: "DIRECTORY",
-            parent: "/",
-            children: [
-              {
-                name: ".bash_history",
-                type: "FILE",
-                file_size: 32819,
-                modified_date: 1674035700000,
-              },
-              {
-                name: "home2",
-                type: "DIRECTORY",
-                parent: "/home",
-                children: [
-                  {
-                    name: ".asd",
-                    type: "FILE",
-                    file_size: 32819,
-                    modified_date: 1674035700000,
-                  },
-                  {
-                    name: "home",
-                    type: "DIRECTORY",
-                    parent: "/home/home2",
-                    children: [
-                      {
-                        name: ".bash_history",
-                        type: "FILE",
-                        file_size: 32819,
-                        modified_date: 1674035700000,
-                      },
-                      {
-                        name: "home2",
-                        type: "DIRECTORY",
-                        parent: "/home/home2/home",
-                        children: [
-                          {
-                            name: ".asd",
-                            type: "FILE",
-                            file_size: 32819,
-                            modified_date: 1674035700000,
-                          },
-                        ],
-                      },
-                    ],
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
+      directories: [
+        {
+          name: "/",
+          type: "DIRECTORY",
+          parent: undefined,
+          children: [
+            {
+              name: "home",
+              type: "DIRECTORY",
+              parent: "/",
+              children: [
+                {
+                  name: ".bash_history",
+                  type: "FILE",
+                  file_size: 32819,
+                  modified_date: 1674035700000,
+                },
+                {
+                  name: "home2",
+                  type: "DIRECTORY",
+                  parent: "/home",
+                  children: [
+                    {
+                      name: ".asd",
+                      type: "FILE",
+                      file_size: 32819,
+                      modified_date: 1674035700000,
+                    },
+                    {
+                      name: "home",
+                      type: "DIRECTORY",
+                      parent: "/home/home2",
+                      children: [
+                        {
+                          name: ".bash_history",
+                          type: "FILE",
+                          file_size: 32819,
+                          modified_date: 1674035700000,
+                        },
+                        {
+                          name: "home2",
+                          type: "DIRECTORY",
+                          parent: "/home/home2/home",
+                          children: [
+                            {
+                              name: ".asd",
+                              type: "FILE",
+                              file_size: 32819,
+                              modified_date: 1674035700000,
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
     },
     {
       id: 2,
       serverName: "Server #2",
-      directories: {
-        name: "/",
-        type: "DIRECTORY",
-        parent: undefined,
-        children: [],
-      },
+      directories: [
+        {
+          name: "/",
+          type: "DIRECTORY",
+          parent: undefined,
+          children: [],
+        },
+      ],
     },
     {
       id: 3,
       serverName: "Server #3",
-      directories: {
-        name: "/",
-        type: "DIRECTORY",
-        parent: undefined,
-        children: [],
-      },
+      directories: [
+        {
+          name: "/",
+          type: "DIRECTORY",
+          parent: undefined,
+          children: [],
+        },
+      ],
     },
     {
       id: 4,
       serverName: "Server #4",
-      directories: {
-        name: "/",
-        type: "DIRECTORY",
-        parent: undefined,
-        children: [],
-      },
+      directories: [
+        {
+          name: "/",
+          type: "DIRECTORY",
+          parent: undefined,
+          children: [],
+        },
+      ],
     },
   ],
 };
