c109df0534d7b992a9edbaa6dd7219ee3eb242ed
chspower1 <chspower1@gmail.com>
2023-02-04 16:08:55 +0900

    style(Home) : Home css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { Col, Row } from "../assets/style/common";
+import { theme } from "../assets/style/theme";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 import { setCurrentDir } from "../store/slice/currentInfoSlice";
 
@@ -16,13 +19,43 @@ const Home = () => {
     );
   }, []);
   return (
-    <>
-      {data.map(({ id }) => (
-        <Link to={`server/${id}`} key={id}>
-          <div>server #{id}</div>
-        </Link>
-      ))}
-    </>
+    <HomeWrapper>
+      <HomeTitle>서버를 선택해주세요!</HomeTitle>
+      <ServerBox>
+        {data.map(({ id }) => (
+          <Link to={`server/${id}`} key={id}>
+            <Server>server #{id}</Server>
+          </Link>
+        ))}
+      </ServerBox>
+    </HomeWrapper>
   );
 };
 export default Home;
+const HomeWrapper = styled(Col)`
+  width: 100vw;
+  height: 100vh;
+`;
+const HomeTitle = styled.h1`
+  font-size: 32px;
+`;
+const ServerBox = styled(Row)`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100vw;
+  height: 500px;
+  gap: 20px;
+`;
+const Server = styled(Row)`
+  width: 200px;
+  height: 200px;
+  background-color: ${({ theme }) => theme.main};
+  color: white;
+  border-radius: 10px;
+  transition: all 0.3s ease;
+  &:hover {
+    background-color: ${({ theme }) => theme.mainHover};
+    width: 210px;
+    height: 210px;
+  }
+`;
